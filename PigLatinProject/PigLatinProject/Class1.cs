68da9b33aec23cfdeecd425c660b41a33507c26e
hsagehorn18 <50709396+hsagehorn18@users.noreply.github.com>
2019-11-26 10:32:26 -0600

    Add files via upload

---
+++ PigLatinProject/PigLatinProject/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatinProject
+{
+    public class PigLatin
+    {
+        public string PigLatinTranslator(string word)
+        {
+            string translation = "";
+            word = word.ToUpper();
+
+            if (word[0] != 'A' && word[0] != 'E' && word[0] != 'I' && word[0] != 'O' && word[0] != 'U')
+            {
+                if (word[1] != 'A' && word[1] != 'E' && word[1] != 'I' && word[1] != 'O' && word[1] != 'U')
+                {
+                    word = word.ToLower();
+                    char firstLetters = word[0];
+                    char secondLetter = word[1];
+                    string temp = word.Substring(2);
+                    translation = temp + "-" + (firstLetters + secondLetter + "ay");
+                }
+                else
+                {
+                    word = word.ToLower();
+                    char firstLetters = word[0];
+                    string temp = word.Substring(1);
+                    translation = temp + "-" + (firstLetters + "ay");
+                }
+            }
+            
+            return translation;
+        }
+        
+    }
+}
---
+++ PigLatinProject/PigLatinTests/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PigLatinProject;
+
+namespace PigLatinTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BasicPigLatinTest()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "hello";
+            string actual = "";
+            string expected = "ello-hay";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BasicPigLatinTest2()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "switch";
+            string actual = "";
+            string expected = "itch-sway";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void BasicPigLatinTest3()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "banana";
+            string actual = "";
+            string expected = "anana-bay";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void BasicPigLatinTest4()
+        {
+            PigLatin PigLatin = new PigLatin();
+            string word = "climb";
+            string actual = "";
+            string expected = "imb-clay";
+
+            actual = PigLatin.PigLatinTranslator(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
